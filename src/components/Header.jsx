8909c4843ef48936154f493901dca17a111454cd
AbishekSathiyan <abidinesh2213.gmail.com>
2025-08-02 15:34:29 +0530

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes, FaChevronDown } from "react-icons/fa";
 
 export default function Header() {
